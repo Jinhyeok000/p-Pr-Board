967080895b0c77cc10adfdc5e6c5d81bb79fa7d3
Jin <sebgokti4276@naver.com>
2024-06-10 11:42:23 +0900

    index updated

---
+++ src/main/java/controller/RegisterController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import commons.util;
+import dao.MemberDAO;
+import dto.MemberDTO;
+
+@WebServlet("*.register")
+public class RegisterController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String cmd = request.getRequestURI();
+		MemberDAO dao = MemberDAO.getInstance();
+		Gson g = new Gson();
+		System.out.println(cmd);
+
+		try {
+			if (cmd.equals("/register.register")) {
+				String id = request.getParameter("id");
+				String pw = util.getSHA512(request.getParameter("pw"));
+				String name = request.getParameter("name");
+				String phone = request.getParameter("phone");
+				String email = request.getParameter("email");
+				String gender = request.getParameter("gender");
+				Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+				String profile_img = request.getParameter("profile_img");
+				int user_level = Integer.parseInt(request.getParameter("user_level"));
+				int result = dao.registerMember(
+						new MemberDTO(id, pw, name, phone, email, gender, currentTime, profile_img, user_level));
+
+				response.sendRedirect("/index.jsp");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("error.jsp");
+		}
+
+	}
+}
