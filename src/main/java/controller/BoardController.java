b6614e726ff9401d73cb62508275bfec488a574a
JKH <jkhkorea28@daum.net>
2024-06-10 11:16:52 +0900

    mainBoard work

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -20,10 +20,29 @@ public class BoardController extends HttpServlet {
 		BoardDAO manager = BoardDAO.getInstance();
 		
 		try {
+			//메인페이지 각 페이지에 표시될 게시글 로드
 			if(cmd.equals("/list.board")){
-				List<BoardDTO> list = manager.getList();
+				String cpage = request.getParameter("cpage");
+				if (cpage == null) {
+					cpage = "1";
+				}
+				int cpageInt = Integer.parseInt(cpage);
 				
+				int recordCountPerPage = 10;
+				int naviCountPerPage = 10;
 				
+				int startNum = cpageInt * recordCountPerPage - (recordCountPerPage-1);
+				int endNum = cpageInt * recordCountPerPage;
+				
+				List<BoardDTO> list = manager.getList(startNum, endNum);
+				
+				request.setAttribute("cpage", cpageInt);
+				request.setAttribute("record_count_per_page", recordCountPerPage);
+				request.setAttribute("navi_count_per_page", naviCountPerPage);
+				request.setAttribute("record_total_count", manager.getList().size());
+				request.setAttribute("list", list);
+				
+				request.getRequestDispatcher("/board/mainBoard.jsp").forward(request, response);
 			}
 			
 			
